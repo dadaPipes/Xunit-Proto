514ad5b70baa6e85eca3e3cfc16e4e2dabbbb92e
dadaPipes <markoliverstockflethstaf@gmail.com>
2025-03-14 01:17:00 +0100

    Brain Fart
    Accidently places the Preprocessor in the wrong
    folder.
    It is not places in the solution folder, so it
    is shipped within the solution.

---
+++ Preprocessor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Preprocessor;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string folderPath = Path.Combine(currentDirectory, "..", "Xunit-Proto", "v3", "v3-2.0.0", "src", "xunit.v3.assert", "Asserts");
+
+        RenameClassNamesToMatchFileNames(folderPath);
+    }
+    static void RenameClassNamesToMatchFileNames(string folderPath)
+    {
+        foreach (string file in Directory.GetFiles(folderPath, "*.cs"))
+        {
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+            string content = File.ReadAllText(file);
+
+            // Replace class name
+            string updatedFileContent = Regex.Replace(content, @"partial class Assert", $"class {fileNameWithoutExtension}");
+
+            File.WriteAllText(file, updatedFileContent);
+            Console.WriteLine($"Updated class name in {file}");
+        }
+
+        Console.WriteLine("Class renaming complete.");
+    }
+}
