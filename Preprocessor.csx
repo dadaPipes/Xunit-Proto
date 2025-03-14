05d21e0f5f49d8ef72cc818dcec99f5b9828a635
dadaPipes <markoliverstockflethstaf@gmail.com>
2025-03-14 04:12:38 +0100

    Done

---
+++ Preprocessor.csx
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+const string folderPath = @"v3\v3-2.0.0\src\xunit.v3.assert\Asserts";
+ChangeClassNamesToFileNames(folderPath);
+
+void ChangeClassNamesToFileNames(string folderPath)
+{
+    foreach (string file in Directory.GetFiles(folderPath, "*.cs"))
+    {
+        string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+        string content = File.ReadAllText(file);
+
+        // Replace class name
+        string updatedFileContent = content.Replace("partial class Assert", $"class {fileNameWithoutExtension}");
+
+        File.WriteAllText(file, updatedFileContent);
+        Console.WriteLine($"Updated class name in {file}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Xunit_Proto
+﻿using System.Text.RegularExpressions;
+
+namespace Xunit_Proto;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        ChangeClassNamesToFileNames(@"v3\v3-2.0.0\src\xunit.v3.assert\Asserts");
+    }
+
+    static void ChangeClassNamesToFileNames(string folderPath)
+    {
+        foreach (string file in Directory.GetFiles(folderPath, "*.cs"))
         {
-            Console.WriteLine("Hello, World!");
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+            string content = File.ReadAllText(file);
+
+            // Replace class name
+            string updatedFileContent = Regex.Replace(content, @"partial class Assert", $"class {fileNameWithoutExtension}");
+
+            File.WriteAllText(file, updatedFileContent);
+            Console.WriteLine($"Updated class name in {file}");
         }
     }
 }
